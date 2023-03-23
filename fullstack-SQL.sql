ef420a7915d051b9f23042c265ba3a508fcb0742
miraj <miraj00@gmail.com>
2023-03-23 11:09:03 -0400

    restaurant field added

--- fullstack-SQL.sql
+++ fullstack-SQL.sql
@@ -2,14 +2,15 @@ create database fullStack;
 create table orders(
 	Id INT PRIMARY KEY AUTO_INCREMENT,
     description varchar(30), 
+    restaurant varchar(30),
     rating INT, 
     orderAgain Boolean
 );
 
-insert into orders (description, rating, orderAgain)
-values('Pasta Primavera', 8, true), 
-('Pizza', 10,true), 
-('sushi', 11, true), 
-('Pasta Alfredo', 1, false);
+insert into orders (description, restaurant, rating, orderAgain)
+values('Italian style Pasta favs', 'Pasta Primavera', 8, true), 
+('Your Local Pizza Place',' Rishi Pizza', 10,true), 
+('Japanese Favorite','sushi', 11, true), 
+('Famous for Alfredo Sauce','Pasta Alfredo', 1, false);
 
 select * from orders;
--- src/main/java/com/fullstackangular/backEndRestaurantFaves/Order.java
+++ src/main/java/com/fullstackangular/backEndRestaurantFaves/Order.java
@@ -14,6 +14,7 @@ public class Order {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String description;
+	private String restaurant;
 	private int rating;
 	private boolean orderAgain;
 	
@@ -21,42 +22,61 @@ public class Order {
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
+	public String getRestaurant() {
+		return restaurant;
+	}
+
+	public void setRestaurant(String restaurant) {
+		this.restaurant = restaurant;
+	}
+
 	public int getRating() {
 		return rating;
 	}
+
 	public void setRating(int rating) {
 		this.rating = rating;
 	}
+
 	public boolean isOrderAgain() {
 		return orderAgain;
 	}
+
 	public void setOrderAgain(boolean orderAgain) {
 		this.orderAgain = orderAgain;
 	}
+
 	
 	// Empty constructor
 	public Order() {
-				
+		
 	}
-	
+
 	// Constructor with fields
-	public Order(Long id, String description, int rating, boolean orderAgain) {
+	public Order(Long id, String description, String restaurant, int rating, boolean orderAgain) {
 		super();
 		this.id = id;
 		this.description = description;
+		this.restaurant = restaurant;
 		this.rating = rating;
 		this.orderAgain = orderAgain;
 	}
 	
+	
+
 	
 	
 }
