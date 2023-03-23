b863de4ac9ccbb39dd4442cff60a7998a5bc1c6d
miraj <miraj00@gmail.com>
2023-03-23 12:07:20 -0400

    get-post and delete functions added and are functional

--- src/main/java/com/fullstackangular/backEndRestaurantFaves/OrderController.java
+++ src/main/java/com/fullstackangular/backEndRestaurantFaves/OrderController.java
@@ -3,8 +3,14 @@ package com.fullstackangular.backEndRestaurantFaves;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -24,7 +30,20 @@ public class OrderController {
 		return repo.findAll();
 	}
 	
+	// (C)RUD -- Create
+	@PostMapping("/orders")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Order create(@RequestBody Order order) {
+		repo.save(order);
+		return order;
+	}
 	
+	
+	@DeleteMapping("/orders/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable("id") Long id) {
+		repo.deleteById(id);
+	}
 	
 	
 }
